9aefee769818e6758f7a35cdadf846c67459b0d1
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-08 23:08:18 +0530

    learning

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { BookService } from './book.service';
+import { Book } from './model/book.model';
 
-@Resolver()
-export class BookResolver {}
+@Resolver( () => Book)
+export class BookResolver {
+    constructor (private readonly bookService : BookService) {}
+
+    @Query( () => [Book])
+    getAllBooks() {
+        return this.bookService.findAll();
+    }
+
+}
