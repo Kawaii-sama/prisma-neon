a10c621256ade9099a3a9fa6e93d2afa033a2063
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-08 23:01:33 +0530

    learning

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBookInput } from './dto/create-book.input';
+import { UpdateBookInput } from './dto/update-book.input';
 
 @Injectable()
 export class BookService {
@@ -23,5 +24,12 @@ export class BookService {
 
     findOne(id: string) {
         return this.prisma.book.findUnique({where :  { id }});
+    }
+
+    update ( data : UpdateBookInput) {
+        return this.prisma.book.update({
+            where : { id: data.id },
+            data : {title : data.title, author: data.author}
+        })
     }
 }
