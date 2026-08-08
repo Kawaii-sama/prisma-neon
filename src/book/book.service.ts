ee4d2961fbc23bbf899571392bcd289d965e4906
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-08 22:55:20 +0530

    learning

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBookInput } from './dto/create-book.input';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(private prisma: PrismaService) {}
+
+    create(data : CreateBookInput) {
+        return this.prisma.book.create({ data });
+    }
+
+    /*
+    1. data would get value from input
+    2. graphQl through resolver would give the value to method
+    3. method would forward to prisma.
+    4. prisma would create the record.
+    */
+}
