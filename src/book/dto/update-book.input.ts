3763a637f130d90161538fde2873d11d82435094
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-08 22:42:58 +0530

    learning

--- src/book/dto/update-book.input.ts
+++ src/book/dto/update-book.input.ts
@@ -1,4 +1,5 @@
-import {InputType, Field, Partial} from '@nestjs/graphql';
+import {InputType, Field, PartialType} from '@nestjs/graphql';
+import { CreateBookInput } from './create-book.input';
 
 @InputType()
 export class UpdateBookInput extends PartialType(CreateBookInput){
