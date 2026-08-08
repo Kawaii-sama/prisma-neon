18baf597f93bd336a23b78947f86de6effcdb230
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-08-08 22:34:43 +0530

    learning

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookModule } from './book/book.module';
+import { join } from 'generated/prisma/internal/prismaNamespaceBrowser';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>
   ({
-
+    driver: ApolloDriver,
+    autoSchemaFile : join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+    playground: true
   })
   ,PrismaModule, BookModule],
   controllers: [AppController],
